8579021c1a0ad7a73bb702958e90f084e701fd4b
Fabricio Mendonca <fabriciomendonca@gmail.com>
2017-03-31 09:30:46 -0300

    more hashing playground with JWT

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,16 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-var message = 'I am user number 3';
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+var decoded = jwt.verify(token, '123abc');
+
+console.log(decoded);
+
+/*var message = 'I am user number 3';
 var hash = SHA256(message).toString();
 
 console.log(`Message: ${message}`);
@@ -24,4 +34,4 @@ if (resultHash === token.hash) {
   console.log('Data was not changed');
 } else {
   console.log('Data was changed. Do not trust!');
-}
+}*/
